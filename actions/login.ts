8596ec2240af4b479cbebc3a161b17a4c8805e2e
HabibRazek <habibrazeg32@gmail.com>
2024-01-28 18:10:17 +0100

    2FA

--- actions/login.ts
+++ actions/login.ts
@@ -4,9 +4,13 @@ import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
-import { generateVerificationToken } from "@/lib/tokens";
+import { generateVerificationToken, generateTwoFactorToken } from "@/lib/tokens";
 import { getUserByEmail } from "@/data/user";
-import { sendVerificationEmail } from "@/lib/mail";
+import { sendVerificationEmail, sendTowFactorTokenEmail } from "@/lib/mail";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
+import { db } from "@/lib/db";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validateFields = LoginSchema.safeParse(values);
@@ -15,23 +19,65 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid credentials !" }
   }
 
-  const { email, password } = validateFields.data;
+  const { email, password, code } = validateFields.data;
 
 
 
   const existingUser = await getUserByEmail(email);
 
-  if (!existingUser || !existingUser.password || !existingUser.email){
+  if (!existingUser || !existingUser.password || !existingUser.email) {
     return { error: "Invalid credentials !" }
   }
 
-  if (!existingUser.emailVerified){
+  if (!existingUser.emailVerified) {
     const verificationToken = await generateVerificationToken(existingUser.email,);
 
-    await sendVerificationEmail(verificationToken.email,verificationToken.token);
+    await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
-    return{
-      success:"Confirmation email sent !",
+    return {
+      success: "Confirmation email sent !",
+    }
+  }
+
+  if (existingUser.isTwoFactorEnabled && existingUser.email) {
+
+    if (code) {
+      const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+      if (!twoFactorToken) return { error: "Invalid code !" }
+      if (twoFactorToken.token !== code) return { error: "Invalid code !" }
+
+      const hasExpired = new Date(twoFactorToken.expires)< new Date();
+      if (hasExpired) return { error: "Code has expired !" }
+
+      await db.twoFactorToken.delete({
+        where: { id: twoFactorToken.id },
+      });
+
+
+      const existingConfirmation = await getTwoFactorConfirmationByUserId (
+        existingUser.id,
+      );
+
+      if (existingConfirmation) {
+        await db.twoFactorConfirmation.delete({
+          where: { id: existingConfirmation.id },
+        });
+      }
+
+      await db.twoFactorConfirmation.create({
+        data: {
+          userId: existingUser.id,
+        },
+      });
+
+
+    } else {
+      const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+      await sendTowFactorTokenEmail(
+        twoFactorToken.email,
+        twoFactorToken.token,
+      );
+      return { twoFactor: true }
     }
   }
 
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -26,8 +26,9 @@ export const LoginForm = () => {
   const searchParams = useSearchParams();
   const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? "Email already in use" : "" ;
 
-  const [isPending, startTransition] = useTransition();
 
+  const [showTwoFactor, setShowTwoFactor] = useState(false);
+  const [isPending, startTransition] = useTransition();
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
 
@@ -50,91 +51,121 @@ export const LoginForm = () => {
     startTransition(() => {
       login(values)
       .then((data) => {
-          setError(data?.error);
-          setSuccess(data?.success);
+          if (data?.error){
+            form.reset();
+            setError(data.error);
+          }
+
+          if (data?.success){
+            form.reset();
+            setSuccess(data.success);
+          }
+
+          if(data?.twoFactor){
+            setShowTwoFactor(true);
+          }
         }
         )
+        .catch(()=> setError("Something went wrong"))
     })
   }
 
 
   return (
-    <div>
-      <CardWrapper
-        headerLabel="Welcome Back!"
-        backButtonLabel=" Don't have an account? Sign up "
-        backButtonHref="/auth/register"
-        showSocial
-      >
-
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className=" space-y-6 "
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6"
+        >
+          <div className="space-y-4">
+            {showTwoFactor && (
+              <FormField
+                control={form.control}
+                name="code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Two Factor Code</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="123456"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
+            {!showTwoFactor && (
+              <>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          disabled={isPending}
+                          placeholder="john.doe@example.com"
+                          type="email"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          disabled={isPending}
+                          placeholder="******"
+                          type="password"
+                        />
+                      </FormControl>
+                      <Button
+                        size="sm"
+                        variant="link"
+                        asChild
+                        className="px-0 font-normal"
+                      >
+                        <Link href="/auth/reset">
+                          Forgot password?
+                        </Link>
+                      </Button>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+            </>
+          )}
+          </div>
+          <FormError message={error || urlError} />
+          <FormSuccess message={success} />
+          <Button
+            disabled={isPending}
+            type="submit"
+            className="w-full"
           >
-            <div className=" space-y-4 ">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Email
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        disabled={isPending}
-                        type="email"
-                        placeholder="exemple@gmail.com"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Password
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        disabled={isPending}
-                        {...field}
-                        type="password"
-                        placeholder="*******"
-                      />
-                    </FormControl>
-                    <Button variant="link" size="sm" className="px-0 font-normal" >
-                      <Link href="/auth/forgot-password">
-                        Forgot password ?
-                      </Link>
-                    </Button>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <FormError message={error || urlError} />
-            <FormSuccess message={success} />
-
-            <Button  type="submit"
-              className="w-full"
-              disabled={isPending}
-            >
-              Login
-            </Button>
-          </form>
-        </Form>
-
-      </CardWrapper>
-    </div>
+            {showTwoFactor ? "Confirm" : "Login"}
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
   )
 }
 
