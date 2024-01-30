b64efb243d1d24176ce0bedeaaefbd99e48f5dba
HabibRazek <habibrazeg32@gmail.com>
2024-01-30 17:23:19 +0100

    Server & Client example

--- auth.ts
+++ auth.ts
@@ -56,9 +56,15 @@ export const {
         session.user.id = token.sub;
       }
 
-      if ( token.sub && session.user){
+      if ( token.role && session.user){
         session.user.role = token.role as UserRole;
       }
+
+      if (session.user){
+        session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
+
       return session;
     },
 
@@ -71,6 +77,7 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
       return token;
     }
