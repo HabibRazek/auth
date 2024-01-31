e575ed024ffe83ec4392077310d1f59e3c2f302a
HabibRazek <habibrazeg32@gmail.com>
2024-01-31 10:28:56 +0100

    - 👑 Render content for admins using RoleGate component - 🛡️ Protect API Routes for admins only - 🔐 Protect Server Actions for admins only

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { auth } from '@/auth'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default async function RootLayout({
       <body className={inter.className}>
       <div className="bg-[#8af2d2] absolute top-[-6rem] -z-10 right-[11rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#8af2d2]"></div>
         <div className="bg-[#aab2ff] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#aab2ff]"></div>
+        <Toaster />
         {children}
         </body>
     </html>
